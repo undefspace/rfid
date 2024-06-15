14bdba54bb4b88ef69600978bd87918625f39521
Anna Antonenko <portasynthinca3@gmail.com>
2024-06-15 16:45:36 +0300

    chore: use led encoder from example, remove now unneeded esp-idf-lib

--- main/include/led_task.h
+++ main/include/led_task.h
@@ -2,14 +2,12 @@
 
 #include <esp_err.h>
 #include <freertos/queue.h>
-#include <rgb.h>
 #include <stdbool.h>
 
 #define LED_Q_SIZE      4
 #define LED_PATTERN_LEN 4
 extern QueueHandle_t led_queue;
 
-#define LED_COLOR_HEX(hex) ((rgb_t){ .r = ((hex) >> 16) & 0xff, .g = ((hex) >> 8) & 0xff, .b = (hex) & 0xff })
 
 typedef enum {
     led_status_error = 0,
@@ -20,7 +18,7 @@ typedef enum {
 } led_status_t;
 
 typedef struct {
-    rgb_t color;
+    uint32_t color;
     uint32_t time_ms;
     bool final;
 } led_pattern_step_t;
--- main/led_task.c
+++ main/led_task.c
@@ -1,35 +1,36 @@
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
-#include <esp_random.h>
-#include <led_strip.h>
-#include <rgb.h>
+#include <driver/rmt.h>
+#include <driver/rmt_tx.h>
 #include <esp_timer.h>
 #include <time.h>
 
 #include "common.h"
 #include "led_task.h"
 #include "config.h"
+#include "led_strip_encoder.h"
 
 #define TAG "led_task"
+#define RMT_LED_STRIP_RESOLUTION_HZ 10000000
 
 static const led_pattern_step_t _led_patterns[][LED_PATTERN_LEN] = {
     /* error */ {
-        { LED_COLOR_HEX(0xff0000), 500, 0 },
-        { LED_COLOR_HEX(0), 500, 1 }
+        { 0xff0000, 500, 0 },
+        { 0x000000, 500, 1 }
     },
     /* wait */ {
-        { LED_COLOR_HEX(0xffff00), 10000, 1 }
+        { 0xffff00, 10000, 1 }
     },
     /* idle */ {
-        { LED_COLOR_HEX(0x00ffff), 20, 0 },
-        { LED_COLOR_HEX(0), 1980, 1 }
+        { 0x00ffff, 20, 0 },
+        { 0x000000, 1980, 1 }
     },
     /* granted */ {
-        { LED_COLOR_HEX(0x00ff00), 10000, 1}
+        { 0x00ff00, 10000, 1}
     },
     /* refused */ {
-        { LED_COLOR_HEX(0xff0000), 10000, 1}
+        { 0xff0000, 10000, 1}
     },
 };
 
@@ -37,14 +38,24 @@ QueueHandle_t led_queue;
 
 void led_task(void* _arg) {
     // initialize LED strip
-    led_strip_t strip = {
-        .type = LED_STRIP_WS2812,
-        .brightness = 255,
-        .length = 1,
-        .gpio = LED_PIN,
-        .channel = RMT_CHANNEL_0,
+    rmt_channel_handle_t led_chan = NULL;
+    rmt_tx_channel_config_t tx_chan_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT,
+        .gpio_num = LED_PIN,
+        .mem_block_symbols = 64,
+        .resolution_hz = RMT_LED_STRIP_RESOLUTION_HZ,
+        .trans_queue_depth = 4,
     };
-    ESP_ERROR_CHECK(led_strip_init(&strip));
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &led_chan));
+    rmt_encoder_handle_t led_encoder = NULL;
+    led_strip_encoder_config_t encoder_config = {
+        .resolution = RMT_LED_STRIP_RESOLUTION_HZ,
+    };
+    ESP_ERROR_CHECK(rmt_new_led_strip_encoder(&encoder_config, &led_encoder));
+    ESP_ERROR_CHECK(rmt_enable(led_chan));
+    rmt_transmit_config_t tx_config = {
+        .loop_count = 0,
+    };
 
     // sequencer state
     led_status_t status = led_status_wait;
@@ -59,11 +70,17 @@ void led_task(void* _arg) {
         }
 
         // refresh led according to next step
+        // discuss: does this macro trick improve or hurt readability?
         #define step _led_patterns[status][step_idx]
         if(step_idx < 0 || esp_timer_get_time() - step_started >= step.time_ms * 1000) {
             step_idx = (step_idx < 0 || step.final) ? 0 : (step_idx + 1);
-            led_strip_set_pixel(&strip, 0, step.color);
-            led_strip_flush(&strip);
+            uint8_t grb[3] = {
+                (step.color & 0x00ff00) >> 8,
+                (step.color & 0xff0000) >> 16,
+                step.color & 0x0000ff
+            };
+            ESP_ERROR_CHECK(rmt_transmit(led_chan, led_encoder, grb, sizeof(grb), &tx_config));
+            ESP_ERROR_CHECK(rmt_tx_wait_all_done(led_chan, portMAX_DELAY));
             step_started = esp_timer_get_time();
         }
         #undef step
@@ -72,7 +89,6 @@ void led_task(void* _arg) {
 
 void led_init(void) {
     led_queue = xQueueCreate(LED_Q_SIZE, sizeof(led_status_t));
-    led_strip_install();
 }
 
 void led_set_status(led_status_t status) {
